64c1d874b6e6980a51a83afabe33af3656dc260f
amanda <amanda@actualize.co>
2022-03-24 12:31:42 -0500

    adds in associations for product, category, and category_product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   has_many :orders
+  belongs_to :supplier
+  has_many :images
+  has_many :category_products
   # validates :name, presence: true
   # validates :name, uniqueness: true
   # validates :price, presence: true
@@ -7,15 +10,6 @@ class Product < ApplicationRecord
   # validates :description, presence: true
   # validates :description, length: { in: 10..500 }
   
-  belongs_to :supplier
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
-
-  has_many :images
-  # def images
-  #   Image.where(product_id: id)
-  # end 
 
   def is_discounted?
     price <= 10
