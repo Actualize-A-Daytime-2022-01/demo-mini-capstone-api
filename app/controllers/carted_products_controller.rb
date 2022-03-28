ecdeb0baf0b03a8faf4282b11a8b25c12474dbb2
amanda <amanda@actualize.co>
2022-03-28 13:07:10 -0500

    adds carted_products index route and action

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -10,4 +10,9 @@ class CartedProductsController < ApplicationController
     carted_product.save
     render json: carted_product
   end
+
+  def index
+    carted_products = current_user.carted_products.where(status: "carted")
+    render json: carted_products
+  end
 end
